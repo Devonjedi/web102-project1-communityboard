34ea9e13ade6f0e5ba3172fa867c9e94d5b022d7
Devonjedi <devon.jedi@gmail.com>
2025-06-04 19:40:39 -0500

    Initial commit

---
+++ react.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  // Log to console
+  console.log('hello work');
+
+  // Return JSX to render
+  return (
+    <div>
+      <h1>hello work</h1>
+    </div>
+  );
+}
+
+export default App;
